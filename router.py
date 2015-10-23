4d74cf95bcde6ce661b33752b6bbe3144b903736
Manasa <mashok@caltech.edu>
2015-10-22 20:34:37 -0700

    Added Router class
    Wrote function declarations for router class, which is derived from the
    Device class.

---
+++ router.py
@@ -0,0 +1,25 @@
+import sys
+from device import Device
+
+class Router(Device):
+    """A router
+
+    Attributes:
+        identifier: The unique identification of the router
+    """
+
+    def __init__(self, identifier):
+        Device.__init__(self, identifier)
+
+    # Called internally by routers to forward packets
+    # and called externally by flows to initiate packet sending
+    def send_packet(self, packet):
+        # Get packet destination from packet and decide
+        # where to send packet next
+        # Should calculate routing tables with decentralized 
+        # shortest path algorithm
+        pass
+
+    # Exclusively called by links to deliver packets
+    def receive_packet(self, packet):
+        pass
