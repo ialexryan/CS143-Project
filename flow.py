d6a51ecab590ccce1c5d1f09677c39774220d1fa
Mimi Jiao <mimij2795@gmail.com>
2015-12-06 14:59:28 -0800

    commented on numbering of packet

--- flow.py
+++ flow.py
@@ -39,6 +39,8 @@ class Flow:
 
     def send_a_packet(self):
         if (self.amount > 0):
+            # numbers the packets in descending order
+            # packet is uniquely identified by flow and packet number
             packetID = "P" + str(self.amount / 1024) + self.identifier
             packet = PayloadPacket(packetID, self.source, self.destination)
             self.logger.log_flow_send_packet(self.identifier, packet)
