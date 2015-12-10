5dbcc17832bba2b34843c764bc56c030a612f1cf
Jaden Geller <jaden.geller@gmail.com>
2015-12-09 17:34:28 -0800

    Cancel events in constant time by setting the canceled flag

--- event_queue.py
+++ event_queue.py
@@ -15,23 +15,23 @@ class EventQueue:
     """Schedules `event` to occur at `time` and returns an event identifier."""
     def schedule_event(self, time, event):
         assert time >= self.clock.current_time
-        event_identifier = (time, event)
-        self._priority_queue.put(event_identifier)
-        return event_identifier
+        self._priority_queue.put((time, event))
+        return event
 
     """Schedules `event` to occur at `delay` milliseconds after the current time
        and returns an event identifier."""
     def delay_event(self, delay, event):
         return self.schedule_event(self.clock.current_time + delay, event)
 
-    """Cancels the event with the given `event_identifier`."""
-    def cancel_event(self, event_identifier):
-        self._priority_queue.queue.remove(event_identifier)
-        heapq.heapify(self._priority_queue.queue)
+    """Cancels the event with the given `event_identifier` with O(1) time complexity."""
+    def cancel_event(self, event):
+        event.is_canceled = True
 
     """Removes next event from queue, updates the global time, and returns the event."""
     def dequeue_next_event(self):
-        (time, event) = self._priority_queue.get_nowait()
+        (time, event) = (None, None)
+        while event is None or event.is_canceled:
+            (time, event) = self._priority_queue.get_nowait()
         assert time >= self.clock.current_time
         self.clock.current_time = time
         return event
