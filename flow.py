c858a42ff9094532bcf83448801c1271b0ebcffc
Alex Ryan <alex@alexryan.org>
2015-12-11 18:38:46 -0800

    Remove unused log from flow
    Code no longer ever reaches the else: path because of congestion
    control.

--- flow.py
+++ flow.py
@@ -47,15 +47,13 @@ class Flow:
         self.controller.wake()
 
     def send_a_packet(self, packet_id, duplicate_num):
-        if self.amount > 0:
-            # numbers the packets in ascending order
-            # packet is uniquely identified by flow, packet number, and which
-            # duplicate of the packet it is
-            packet = PayloadPacket(packet_id, duplicate_num, self.identifier, self.source, self.destination, 1024, 64)
-            self.logger.log_flow_send_packet(self.identifier, packet)
-            self.source.send_packet(packet)
-        else:
-            self.logger.log_flow_completed(self.identifier)
+        assert(self.amount > 0)
+        # numbers the packets in ascending order
+        # packet is uniquely identified by flow, packet number, and which
+        # duplicate of the packet it is
+        packet = PayloadPacket(packet_id, duplicate_num, self.identifier, self.source, self.destination, 1024, 64)
+        self.logger.log_flow_send_packet(self.identifier, packet)
+        self.source.send_packet(packet)
 
     # Called by a link's host whenever an acknowledgement is received
     def acknowledgement_received(self, packet):
